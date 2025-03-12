770008cc81c5a9671e533deff889ed280206bbf5
Miguel Silva <98617294+miguel-silva48@users.noreply.github.com>
2025-03-12 17:37:44 +0000

    feat: enhance command-line interface with help menu for fcm and generator.
    This commit also adds the README file with instructions.

--- fcm.cpp
+++ fcm.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void printError() {
+void printHelpMenu() {
     cout << "Usage: ./fcm <input_file> -k <context_size> -a <alpha> -o <output_model> [--json]\n";
     cout << "Example: ./fcm sequences/sequence1.txt -k 3 -a 0.1 -o model --json\n";
     cout << "\nOptions:\n";
@@ -13,11 +13,24 @@ void printError() {
     cout << "  -a <alpha>     : Smoothing parameter (default: 0.1)\n";
     cout << "  -o <outfile>   : Output file for the model (without extension)\n";
     cout << "  --json         : Save in JSON format (default is binary)\n";
+    cout << "  -h             : Display this help menu\n";
 }
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printHelpMenu();
+        return 1;
+    }
+    
+    // Check for help flag
+    string firstArg = argv[1];
+    if (firstArg == "-h") {
+        printHelpMenu();
+        return 0;
+    }
+
     if (argc < 8) {
-        printError();
+        printHelpMenu();
         return 1;
     }
 
@@ -27,16 +40,34 @@ int main(int argc, char* argv[]) {
     string outputFile = "model";
     bool binary = true; // default to binary format
 
-    for (int i = 2; i < argc; i += 2) {
+    for (int i = 2; i < argc; i++) {
         string arg = argv[i];
-        if (arg == "-k") {
-            k = atoi(argv[i + 1]);
-        } else if (arg == "-a") {
-            alpha = atof(argv[i + 1]);
-        } else if (arg == "-o") {
-            outputFile = argv[i + 1];
-        } else if (arg == "--json") {
+        
+        if (arg == "--json") {
             binary = false;
+            continue;
+        }
+        
+        // Check if there's a next argument for options that require values
+        if (arg == "-k" || arg == "-a" || arg == "-o") {
+            if (i + 1 >= argc) {
+                cerr << "Error: Missing value for argument " << arg << endl;
+                printHelpMenu();
+                return 1;
+            }
+            
+            // Parse arguments with values
+            if (arg == "-k") {
+                k = atoi(argv[++i]);
+            } else if (arg == "-a") {
+                alpha = atof(argv[++i]);
+            } else if (arg == "-o") {
+                outputFile = argv[++i];
+            }
+        } else if (arg != "--json") {
+            cerr << "Error: Unknown option " << arg << endl;
+            printHelpMenu();
+            return 1;
         }
     }
 
